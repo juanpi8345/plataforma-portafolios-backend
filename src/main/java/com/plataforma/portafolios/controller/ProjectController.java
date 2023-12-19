1bd627de49095d41c2c609b9a2a38e6e84c308b2
Juan Pregliasco <juanpipre@outlook.com>
2023-12-19 17:33:05 -0300

    Project controller

--- src/main/java/com/plataforma/portafolios/controller/ProjectController.java
+++ src/main/java/com/plataforma/portafolios/controller/ProjectController.java
@@ -4,6 +4,8 @@ import com.plataforma.portafolios.model.Profile;
 import com.plataforma.portafolios.model.Project;
 import com.plataforma.portafolios.service.IProfileService;
 import com.plataforma.portafolios.service.IProjectService;
+import com.plataforma.portafolios.service.IUserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -21,24 +23,65 @@ public class ProjectController {
     @Autowired
     private IProjectService projectServ;
 
-    @GetMapping("/get/profile/{profileId}")
-    public ResponseEntity<List<Project>> getUserProjects(@PathVariable Long profileId){
-        Profile profile = profileServ.getProfile(profileId);
+    @Autowired
+    private IUserService userServ;
+
+    @GetMapping("/get}")
+    public ResponseEntity<List<Project>> getUserProjects(Principal principal){
+        Profile profile = userServ.getLogedUser(principal).getProfile();
         if(profile!=null)
             return ResponseEntity.ok(profile.getProjects());
         return ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/get/profile/{profileId}/project/{projectId}")
-    public ResponseEntity<Project> getUserProject(@PathVariable Long profileId, @PathVariable Long projectId){
-        Profile profile = profileServ.getProfile(profileId);
+    @GetMapping("/get/project/{projectId}")
+    public ResponseEntity<Project> getUserProject(@PathVariable Long projectId,Principal principal){
+        Profile profile = userServ.getLogedUser(principal).getProfile();
         Project project = projectServ.getProject(projectId);
-
         if(profile!=null && project != null){
-
+            return ResponseEntity.ok(project);
         }
-        return null;
+        return ResponseEntity.notFound().build();
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<Project> addProject(@Valid @RequestBody Project project,Principal principal){
+        Profile profile = userServ.getLogedUser(principal).getProfile();
+        if(profile!=null){
+            profile.getProjects().add(project);
+            project.setProfile(profile);
+            profileServ.saveProfile(profile);
+            return ResponseEntity.ok(project);
+        }
+        return ResponseEntity.notFound().build();
+    }
 
+    @PutMapping("/edit")
+    public ResponseEntity<Project> editProject(@RequestBody Project projectRequest, Principal principal) {
+        Project project = projectServ.getProject(projectRequest.getProjectId());
+        Profile profile = userServ.getLogedUser(principal).getProfile();
+        if(project!= null && profile != null){
+            project.setName(projectRequest.getName());
+            project.setStart(projectRequest.getStart());
+            project.setEnd(projectRequest.getEnd());
+            project.setDescription(projectRequest.getDescription());
+            project.setImage(projectRequest.getImage());
+            profileServ.saveProfile(profile);
+            return ResponseEntity.ok(project);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/delete/project/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable Long projectId, Principal principal){
+        Project project = projectServ.getProject(projectId);
+        Profile profile = userServ.getLogedUser(principal).getProfile();
+        if(project != null && profile!=null){
+            profile.getProjects().remove(project);
+            projectServ.deleteProject(projectId);
+            profileServ.saveProfile(profile);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
